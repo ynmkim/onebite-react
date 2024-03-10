a19ca30a77fad1d35dd8f08cdce27eeb0e479cb4
김윤미 <younmeet@gmail.com>
2024-03-11 00:10:12 +0900

    feat: mockData 추가

--- section08/src/App.jsx
+++ section08/src/App.jsx
@@ -2,8 +2,32 @@ import './App.css';
 import InputForm from './components/InputForm';
 import Header from './components/Header';
 import TodoList from './components/TodoList';
+import { useState } from 'react';
+
+const mockData = [
+  {
+    id: 0,
+    isDone: false,
+    content: '리액트 공부하기',
+    createdAt: new Date().getTime(),
+  },
+  {
+    id: 1,
+    isDone: false,
+    content: '빨래하기',
+    createdAt: new Date().getTime(),
+  },
+  {
+    id: 2,
+    isDone: false,
+    content: '청소하기',
+    createdAt: new Date().getTime(),
+  },
+];
 
 function App() {
+  const [todos, setTodos] = useState(mockData);
+
   return (
     <div className="App">
       <Header />
